9e67fbd4a613b47dfce39150a4266e2c0c67c662
Manuela Castrillon <manuela@skillshare.com>
2021-10-11 18:21:55 -0500

    add hacktoberfest event

--- src/assets/data/talks.js
+++ src/assets/data/talks.js
@@ -1,169 +1,249 @@
 module.exports = [
   {
+    title: 'Hacktoberfest 2021, como contribuir en Open Source',
+    date: 'October 11, 2021',
+    event: 'Codex UdeA',
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url:
+          'https://docs.google.com/presentation/d/1t4PyHUzxnh0MjrV8PRs2CZ3u0SWxXTLobkPqvQ0AxFQ/edit?usp=sharing'
+      }
+    ]
+  },
+  {
     title: 'Intro a Git',
     date: 'September 19, 2020',
     event: 'Pioneras Developers',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://slides.com/manucastrillonm/pioneras-git'
-    }]
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url: 'https://slides.com/manucastrillonm/pioneras-git'
+      }
+    ]
   },
   {
     title: '10 cosas que me hubiera gustado saber cuando empecé a programar',
     date: 'September 12. 2020',
     event: 'Women Tech Makers',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://slides.com/manucastrillonm/wtm'
-    }]
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url: 'https://slides.com/manucastrillonm/wtm'
+      }
+    ]
   },
   {
     title: 'Building a bridge: Experience the industry as a student',
     date: 'August 20, 2020',
     event: 'MLH Hackon',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://docs.google.com/presentation/d/1dNVWf8bAFz8PNNy7uW-6ldzEq5rSz07OT9KuSKd8ELc/edit?usp=sharing'
-    }]
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url:
+          'https://docs.google.com/presentation/d/1dNVWf8bAFz8PNNy7uW-6ldzEq5rSz07OT9KuSKd8ELc/edit?usp=sharing'
+      }
+    ]
   },
   {
     title: 'Creciendo pero juntas',
     date: 'July 24, 2020',
     event: 'WIE USCO',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://docs.google.com/presentation/d/10wTR9eEB61CwfveqwXWot5Hv09E-KOA9zilqHZ_KpYo/edit?usp=sharing'
-    }]
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url:
+          'https://docs.google.com/presentation/d/10wTR9eEB61CwfveqwXWot5Hv09E-KOA9zilqHZ_KpYo/edit?usp=sharing'
+      }
+    ]
   },
   {
     title: 'Building a REST API using Express.js',
     date: 'June 29, 2020',
     event: 'Women Who Code Medellin',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://docs.google.com/presentation/d/1h1Lk7L2x3qksLI_CI22sTTxBb-2T1pqmQqKFew79d9A/edit?usp=sharing'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url:
+          'https://docs.google.com/presentation/d/1h1Lk7L2x3qksLI_CI22sTTxBb-2T1pqmQqKFew79d9A/edit?usp=sharing'
+      }
+    ]
+  },
+  {
     title: 'Construyendo web apps con Gatsby.js',
     date: 'June 20, 2020',
     event: 'Pioneras Developers (Online)',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://docs.google.com/presentation/d/1FUTkUmHMUB8ETiWxci__VKfrGXc-tF2DVk4FbZJ7jvk/edit'
-    }, {
-      name: 'Video [ES]',
-      url: 'https://www.youtube.com/watch?v=vGPuuzPxIeA'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url:
+          'https://docs.google.com/presentation/d/1FUTkUmHMUB8ETiWxci__VKfrGXc-tF2DVk4FbZJ7jvk/edit'
+      },
+      {
+        name: 'Video [ES]',
+        url: 'https://www.youtube.com/watch?v=vGPuuzPxIeA'
+      }
+    ]
+  },
+  {
     title: 'Workflows personalizados utilizando GitHub Actions',
     date: 'March 25, 2020',
     event: 'Medellin.js (Online)',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://slides.com/manucastrillonm/github-actions'
-    }, {
-      name: 'Video [ES]',
-      url: 'https://youtu.be/a2tX5907XEQ?t=1749'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url: 'https://slides.com/manucastrillonm/github-actions'
+      },
+      {
+        name: 'Video [ES]',
+        url: 'https://youtu.be/a2tX5907XEQ?t=1749'
+      }
+    ]
+  },
+  {
     title: 'Git from Zero to Hero',
     date: 'March 7, 2020',
     event: 'Codex UdeA (Medellin)',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://drive.google.com/file/d/1Nacxq9M0MjDzsimHVgkb8d-ABZjLxaoM/view?usp=sharing'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url:
+          'https://drive.google.com/file/d/1Nacxq9M0MjDzsimHVgkb8d-ABZjLxaoM/view?usp=sharing'
+      }
+    ]
+  },
+  {
     title: 'The good, the bad, and the code review',
     date: 'November 15, 2019',
     event: 'GitHub Education summit (San Francisco)',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://slides.com/manucastrillonm/github-education-day'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url: 'https://slides.com/manucastrillonm/github-education-day'
+      }
+    ]
+  },
+  {
     title: 'CNN in the browser using TensorFlow.js',
     date: 'June 19, 2019',
     event: 'Campus Party CO (Bogota)',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://docs.google.com/presentation/d/1kpdVAoX3kGjTzFBKdy1OVvx3qRr7oSwJfHo2prQApE4/edit?usp=sharing'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url:
+          'https://docs.google.com/presentation/d/1kpdVAoX3kGjTzFBKdy1OVvx3qRr7oSwJfHo2prQApE4/edit?usp=sharing'
+      }
+    ]
+  },
+  {
     title: 'A JavaScript Nightmare (coercion)',
     date: 'May 22, 2019',
     event: 'GitHub Campus Experts Meetup (Berlin)',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://slides.com/manucastrillonm/js-coercion#/'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url: 'https://slides.com/manucastrillonm/js-coercion#/'
+      }
+    ]
+  },
+  {
     title: 'One day, one app, prototyping projects using Google Vision API',
     date: 'April 6, 2019',
     event: 'Women In Data Science Conf (Medellin)',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://drive.google.com/file/d/1Oh96X3bgwyiE8bhVVIu_pyFxp1xOJVhX/view?usp=sharing'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url:
+          'https://drive.google.com/file/d/1Oh96X3bgwyiE8bhVVIu_pyFxp1xOJVhX/view?usp=sharing'
+      }
+    ]
+  },
+  {
     title: 'Big Data from Scratch',
     date: 'July 31, 2018',
     event: 'UdeA',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://docs.google.com/presentation/d/1-5ivAD8G4GYElUehyzHuMyEhLEBdcePT63mi_ipK2qE/edit#slide=id.g3f45ad4a1c_2_51'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url:
+          'https://docs.google.com/presentation/d/1-5ivAD8G4GYElUehyzHuMyEhLEBdcePT63mi_ipK2qE/edit#slide=id.g3f45ad4a1c_2_51'
+      }
+    ]
+  },
+  {
     title: 'What the Ho*k (react hooks)',
     date: 'November, 2018',
     event: 'React Medellin',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://slides.com/manucastrillonm/react-hooks#/'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url: 'https://slides.com/manucastrillonm/react-hooks#/'
+      }
+    ]
+  },
+  {
     title: 'Tech conferences: Opportunities and scholarships',
     date: 'October, 2018',
     event: 'Node.js Medellin',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://slides.com/manucastrillonm/tech-conf-scholarships#/'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url: 'https://slides.com/manucastrillonm/tech-conf-scholarships#/'
+      }
+    ]
+  },
+  {
     title: 'CSS Houdini',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://slides.com/manucastrillonm/css-houdini#/'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url: 'https://slides.com/manucastrillonm/css-houdini#/'
+      }
+    ]
+  },
+  {
     title: 'Building a better country trough tech',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://slides.com/manucastrillonm/tech-communities'
-    }]
-  }, {
-    title: 'SASS: Let\'s make CSS great againg',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://slides.com/manucastrillonm/sass-pioneras'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url: 'https://slides.com/manucastrillonm/tech-communities'
+      }
+    ]
+  },
+  {
+    title: "SASS: Let's make CSS great againg",
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url: 'https://slides.com/manucastrillonm/sass-pioneras'
+      }
+    ]
+  },
+  {
     title: 'NgRx',
-    resources: [{
-      name: 'Slides [EN]',
-      url: 'https://slides.com/manucastrillonm/ngrx#/'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [EN]',
+        url: 'https://slides.com/manucastrillonm/ngrx#/'
+      }
+    ]
+  },
+  {
     title: 'Como preparar una pizza con SMACSS',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://slides.com/manucastrillonm/smacss#/'
-    }]
-  }, {
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url: 'https://slides.com/manucastrillonm/smacss#/'
+      }
+    ]
+  },
+  {
     title: 'Introducción a Ionic',
-    resources: [{
-      name: 'Slides [ES]',
-      url: 'https://slides.com/manucastrillonm/ionic-pioneras#/'
-    }]
+    resources: [
+      {
+        name: 'Slides [ES]',
+        url: 'https://slides.com/manucastrillonm/ionic-pioneras#/'
+      }
+    ]
   }
 ]
