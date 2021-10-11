6d824e9e37b7cae79d60be3f4bd376f85ae5f775
Manuela Castrillon <manuela@skillshare.com>
2021-10-11 18:38:47 -0500

    update portfolio

--- src/assets/data/portfolio.js
+++ src/assets/data/portfolio.js
@@ -4,10 +4,17 @@ module.exports = [
     description: 'Website for the first conference about Git in LATAM',
     url: 'https://www.gitconf.co/',
     image: 'portfolio-gitconfco.png'
-  }, {
+  },
+  {
     name: 'Disney API',
-    description: 'REST API based on Disney characters',
+    description: 'REST and GraphQL API based on Disney characters',
     url: 'https://www.disneyapi.dev/',
     image: 'portfolio-disneyapi.png'
+  },
+  {
+    name: 'Codex UdeA',
+    description: "Website for 'Codex UdeA'",
+    url: 'https://codexudea.github.io/',
+    image: 'portfolio-codex.jpg'
   }
 ]
