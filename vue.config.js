b3b82c269dba9f862ed4f3768cf2fa7324136359
Manu Castrillon <manu.cm43@gmail.com>
2022-06-26 22:52:13 -0500

    update public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/manuela.dev/' : '/'
+  publicPath: process.env.BASE_URL
 })
